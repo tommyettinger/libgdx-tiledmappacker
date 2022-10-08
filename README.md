ed0970f0668c6feb113106ef66d3a4ba554991d7
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-10-07 17:07:57 -0700

    First, very rough, draft. Uses PixmapPacker.

---
+++ README.md
@@ -0,0 +1,9 @@
+# libgdx-tiledmappacker
+A standalone version of gdx-tools' TiledMapPacker.
+
+See the Releases section for runnable JARs.
+
+[See the libGDX wiki for more information](https://libgdx.com/wiki/graphics/2d/texture-compression).
+
+This was taken from inside [libGDX](https://github.com/libgdx/libgdx) and moved so it can be run more easily from
+outside that framework. It still has the same license as libGDX.
---
+++ graalvm-env/build_native.bat
@@ -0,0 +1,13 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
+call C:\d\jvm\graal17\bin\native-image.cmd ^
+-H:+ReportExceptionStackTraces ^
+--report-unsupported-elements-at-runtime ^
+--no-fallback ^
+--allow-incomplete-classpath ^
+-H:ReflectionConfigurationFiles=config/reflect-config.json ^
+-H:JNIConfigurationFiles=config/jni-config.json ^
+-H:DynamicProxyConfigurationFiles=config/proxy-config.json ^
+-H:SerializationConfigurationFiles=config/serialization-config.json ^
+-H:ResourceConfigurationFiles=config/resource-config.json ^
+-Dorg.lwjgl.librarypath=. ^
+-jar gdx-tiledmappacker-1.11.0.0.jar
